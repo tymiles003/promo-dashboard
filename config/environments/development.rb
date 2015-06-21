a201806858412581c4ccfc274fcc113b1a715c6a
Ben Greenberg <bsgreenb@gmail.com>
2015-06-21 13:01:25 -0700

    mailer setup and more config shit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,21 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.delivery_method = :smtp
+
+  ActionMailer::Base.smtp_settings = {
+      :user_name => ENV['smtp_username'],
+      :password => ENV['smtp_password'],
+      :address => ENV['smtp_address'],
+      :port => ENV['smtp_port'],
+      :authentication => :plain,
+      :enable_starttls_auto => true
+  }
+
+  config.action_mailer.default_url_options = {:host => 'localhost:3000'}
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
---
+++ test/mailers/previews/r_test_mail_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/r_test_mail
+class RTestMailPreview < ActionMailer::Preview
+
+end
