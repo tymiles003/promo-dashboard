aa8e7deaeb012747f857b18d8c670e7711b19068
Ben Greenberg <bsgreenb@gmail.com>
2016-03-08 08:49:27 -0800

    spec cleanup

--- spec/views/events/index.html.haml_spec.rb
+++ spec/views/events/index.html.haml_spec.rb
@@ -1,26 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe "events/index", type: :view do
-  it 'handles no events case' do
-    events = Event.where("1 = 0")
-    assign(:events, events)
+  context 'user has no events' do
+    before do
+      assign(:events, Event.none)
+    end
 
-    render
+    it 'displays you have no events message' do
+      render
 
-    expect (rendered).to include("You currently have no events")
+      expect (rendered).to include("You currently have no events")
+    end
+
+  end
+
+  context 'user has events' do
+    before do
+      my_first_event = Event.create(title: "First Amazing Event", start_at: Time.now + 5.days, end_at: Time.now + 6.days, eventbrite_event_id: 123, uses_per_code: 1)
+      my_second_event = Event.create(title: "Second Amazing Event", start_at: Time.now + 3.days, end_at: Time.now + 4.days, eventbrite_event_id: 124, uses_per_code: 1)
+
+      events = [my_first_event, my_second_event]
+      assign(:events, events)
+    end
   end
 
   it 'displays the users events' do
-
-    my_first_event = Event.create(title: "First Amazing Event", start_at: Time.now + 5.days, end_at: Time.now + 6.days, eventbrite_event_id: 123, uses_per_code: 1)
-    my_second_event = Event.create(title: "Second Amazing Event", start_at: Time.now + 3.days, end_at: Time.now + 4.days, eventbrite_event_id: 124, uses_per_code: 1)
-
-    events = [my_first_event, my_second_event]
-    assign(:events, events)
-
     render
 
     expect(rendered).to include("First Amazing Event")
     expect(rendered).to include("Second Amazing Event")
   end
+
 end
