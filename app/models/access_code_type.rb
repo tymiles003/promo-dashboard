5bf27d37f6157edacf7c81804fcca9805baaba3d
Ben Greenberg <bsgreenb@gmail.com>
2016-03-09 14:35:34 -0800

    reintroduce default

--- app/models/access_code_type.rb
+++ app/models/access_code_type.rb
@@ -24,6 +24,7 @@ class AccessCodeType < ActiveRecord::Base
   after_initialize :default_values
   def default_values
     self.default_allowance ||= 2
+    self.num_uses_per_code ||= 1
   end
 
 end
